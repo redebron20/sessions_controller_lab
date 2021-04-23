2f1760ad2b86298b2d062ecf5dff1d58cb44d530
redebron20 <redebron20@gmail.com>
2021-04-23 11:21:10 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ActionController::Base
+
+    def new
+    end
+
+    def create
+        if !params[:name] || params[:name].empty?
+            redirect_to controller: 'sessions', action: 'new'
+        else
+            session[:name] = params[:name]
+            redirect_to '/'
+        end
+    end
+
+    def destroy
+        session.delete :name
+    end
+
+end
